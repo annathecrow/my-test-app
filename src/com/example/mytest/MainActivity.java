60c0a10b4d75f5b07c62a405a5c297020d9bc5cf
Anna the Crow <a.frankova.contact@gmail.com>
2013-05-12 21:32:09 +0200

    Finished second lesson from Android Developers tutorial.

--- src/com/example/mytest/MainActivity.java
+++ src/com/example/mytest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mytest;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	public final static String EXTRA_MESSAGE = "com.example.mytest.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,15 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void sendMessage(View view) {
+		//do something
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE,message);
+		startActivity(intent);
 	}
 
 }
